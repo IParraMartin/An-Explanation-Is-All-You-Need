ccbe5652155456c8b833ab34d22a867e9e5932cc
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2024-06-15 14:49:39 -0500

    refactor: Improve MultiHeadAttentionBloc and FeedForwardBlock in model.py

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from torchsummary import summary
 import math
 
 
@@ -10,6 +9,7 @@ We set up the dimension of the embeddings (d_model),
 and the vocab size (vocan_size). Then we use an embedding
 layer from torch.nn and set num_embeddings and embedding_dim
 """
+
 class InputEmbeddings(nn.Module):
     def __init__(self, d_model: int, vocab_size: int):
         super().__init__()
@@ -31,6 +31,7 @@ to encode the information of word order in some way. The
 authors came up with Positional Encoding, wich is summed to 
 the original embedding vectors.
 """
+
 class PositionalEmbeddings(nn.Module):
     def __init__(self, d_model: int, seq_len: int, dropout: float):
         super().__init__()
@@ -41,7 +42,6 @@ class PositionalEmbeddings(nn.Module):
         # Create a tensor of zeroes to fill in the following steps
         pe = torch.zeros(seq_len, d_model)
         # Create a positions vector of shape seq_len. We use .arange(), 
-        # begining at 0 and finising in the max of the seq_len
         position = torch.arange(start=0, end=seq_len, dtype=torch.float).unsqueeze(1)
         # We create the division term of the formula
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000) / d_model))
@@ -51,7 +51,7 @@ class PositionalEmbeddings(nn.Module):
         
         # Now we need to make it applicable to batches. To do so, we need 
         # to add an extra dimension in the first position. We do so by using
-        # unsqueeze at position 0
+        # .unsqueeze at position 0
         pe = pe.unsqueeze(0)
 
         # We need to register the tensor in the buffer of the module (kind of remember it).
@@ -74,6 +74,7 @@ class PositionalEmbeddings(nn.Module):
 We now design the Layer Normalization. We do this by passing an epsilon value (eps), 
 which is added to avoid 0 division in the normalization operation.
 """
+
 class LayerNormalization(nn.Module):
     def __init__(self, eps: float = 10 ** -6):
         super().__init__()
@@ -94,6 +95,7 @@ class LayerNormalization(nn.Module):
 This is the Position Wise Feed-Forward Network. That's just a fancy name
 for a simple neural network, with two layers and a Relu activation in between.
 """
+
 class FeedForwardBlock(nn.Module):
     def __init__(self, d_model: int, d_ff: int, dropout: float):
         super().__init__()
@@ -109,10 +111,10 @@ class FeedForwardBlock(nn.Module):
 """
 This is probably the most important block: the famous Multi-Head Attention.
 """
+
 class MultiHeadAttentionBloc(nn.Module):
     def __init__(self, d_model: int, n_heads: int, dropout: float):
         super().__init__()
-        # We set the model heads
         self.d_model = d_model
         self.n_heads = n_heads
         # We must be able to send equal dims to the different heads
@@ -129,7 +131,6 @@ class MultiHeadAttentionBloc(nn.Module):
         # Set Wo, which is [(n_heads * self.d_v), d_model] which is the same as [d_model, d_model]
         self.w_o = nn.Linear(d_model, d_model)
 
-        # Finally the dropout
         self.dropout = nn.Dropout(dropout)
 
         """
@@ -150,7 +151,6 @@ class MultiHeadAttentionBloc(nn.Module):
         if mask is not None:
             attention_scores.masked_fill_(mask == 0, -1e9)
         # We apply softmax
-        # Transform: [Batch, n_heads, seq_len, seq_len]
         attention_scores = attention_scores.softmax(dim = -1) 
         # We add dropout
         if dropout is not None:
@@ -186,8 +186,11 @@ class MultiHeadAttentionBloc(nn.Module):
         # here we use the function we previously introduced as a static method
         x, self.attention_scores = MultiHeadAttentionBloc.attention(query, key, value, mask, self.dropout)
 
-        # Here we concatenate the information from the different heads
-        # [batch, n_heads, seq_len, d_k] -> [batch, seq_len, n_heads, d_k] -> [batch, seq_len, d_model]
+        # Here we concatenate the information from the different heads. By multiplying n_heads and d_k, 
+        # we effectively concatenate the d_k elements from each head for every sequence position into a single 
+        # dimension, resulting in a tensor where the information from different heads is concatenated 
+        # for each position in the sequence.
+        # Transform: [batch, n_heads, seq_len, d_k] -> [batch, seq_len, n_heads, d_k] -> [batch, seq_len, d_model]
         x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.n_heads * self.d_k)
 
         # [batch, seq_len, d_model] -> [batch, seq_len, d_model]
@@ -207,8 +210,5 @@ class ResidualConnection(nn.Module):
     def forward(self, x, sublayer):
         # Normalize x, then pass it through a sublayer, use the dropout term, and finally add x
         return x + self.dropout(sublayer(self.normalization(x)))
-
-
-
 
 
